29a7eba848f3f97993b1a98a25436a571be4caeb
Cahuete0512 <74617561+Cahuete0512@users.noreply.github.com>
2022-06-17 12:06:08 +0200

    initialisation du git. Premier commit

---
+++ src/main/java/com/cahuete/familyeventfinance/FamilyEventFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.cahuete.familyeventfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyEventFinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyEventFinanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cahuete/familyeventfinance/FamilyEventFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cahuete.familyeventfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyEventFinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
